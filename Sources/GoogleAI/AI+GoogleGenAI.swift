76813b258768463816407e0d9cfbf59438366082
Cristian A Monterroza <1240948+rismay@users.noreply.github.com>
2025-07-21 17:17:24 -0700

    Update to AI+GoogleGenAI.

--- Sources/GoogleAI/AI+GoogleGenAI.swift
+++ Sources/GoogleAI/AI+GoogleGenAI.swift
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import Foundation
+import WrkstrmNetworking
 
 #if !os(macOS) && !os(iOS)
 #warning("Only iOS, macOS, and Catalyst targets are currently fully supported.")
@@ -19,9 +20,32 @@ import Foundation
 
 /// Constants associated with the GenerativeAISwift SDK.
 @available(iOS 15.0, macOS 11.0, macCatalyst 15.0, *)
-public enum GenerativeAISwift {
-  /// String value of the SDK version
-  public static let version = "0.5.6"
-  /// The Google AI backend endpoint URL.
-  static let baseURLString = "https://generativelanguage.googleapis.com"
+enum AI {
+  public enum GoogleGenAI {
+
+    struct Environment: HTTP.Environment, Sendable {
+      static func betaEnv(with apiKey: String) -> Environment {
+        .init(apiKey: apiKey)
+      }
+      
+      var clientVersion: String? = "0.5.6"
+      var scheme: HTTP.Scheme = .https
+      /// The Google AI backend endpoint URL.
+      var baseURLString: String = "generativelanguage.googleapis.com"
+      var apiVersion: String? = "v1beta"
+      var apiKey: String?
+      
+      var headers: HTTP.Client.Headers {
+        [
+          "x-goog-api-key": apiKey ?? "",
+          "x-goog-api-client": "genai-swift/\(String(describing: clientVersion))",
+          "Content-Type": "application/json",
+        ]
+      }
+      
+      init(apiKey: String) {
+        self.apiKey =  apiKey
+      }
+    }
+  }
 }
